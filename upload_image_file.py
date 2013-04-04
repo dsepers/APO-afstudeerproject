7338a7e596549d2b37a6283c61b8351af2e1013e
Daniel Sepers <kidssepers@hotmail.com>
2013-04-04 15:57:46 +0200

    made small python script that prompts user for a filename and inserts that filename into database

---
+++ upload_image_file.py
@@ -0,0 +1,7 @@
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
+from image_fits_to_db import insert_image_into_db
+
+Tk().withdraw() # suppress GUI
+filename = askopenfilename() # show prompt to select a file name
+insert_image_into_db(filename) # insert file into db
